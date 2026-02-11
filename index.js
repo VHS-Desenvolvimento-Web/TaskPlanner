b0cd09f8fb7831b9a32850494b7917e239d124e2
Victor Henrique Souza <vhs.desenvolvedorweb@gmail.com>
2026-02-10 22:57:48 -0300

    update 6

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 const mensagem = document.querySelector('#mensagem')
 const botaoLimpar = document.querySelector('#botaoLimpar')
+const inputTarefa = document.querySelector('#inputTarefa')
 let tarefas = []
 
-function adicionarTarefa() {
-    const inputTarefa = document.querySelector('#inputTarefa')
-    let tarefa = inputTarefa.value.trim()
+const listaSalva = localStorage.getItem('tarefas');
+if (listaSalva !== null) {
+    tarefas = JSON.parse(listaSalva)
+}
 
+if (tarefas.length > 0) {
+        console.log('as tarefas foram recarregadas com o refresh do site.')
+    }
+
+inputTarefa.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        adicionarTarefa();
+    }
+});
+
+renderizarTarefas()
+atualizarListaStorage()
+
+function adicionarTarefa() {
+    let tarefa = inputTarefa.value.trim()
     if (tarefa == '') {
         mudarMensagemErro();
-        setTimeout(() => {mensagem.style.opacity = '0'},  2500)
     } else {
         mudarMensagemSucesso()
-        setTimeout(() => {mensagem.style.opacity = '0'},  2500)
-        tarefas.push(tarefa)
+        tarefas.push({
+            texto: tarefa,
+            prazo: '',
+            status: false
+        })
         renderizarTarefas()
         mostrarBotao()
         console.log(tarefa)
+        atualizarListaStorage()
     }
-
     inputTarefa.value = ''
 }
 
@@ -26,9 +45,33 @@ function renderizarTarefas() {
     listaTarefas.innerHTML = '' // esvazia a lista para quando ela for reescrita, não repetir as mesmas tarefas
 
     for (let i = 0; i < tarefas.length; i++) {
-        let novaTarefa = document.createElement('li')
+        const novaTarefa = document.createElement('li')
         novaTarefa.className = 'nova-tarefa'
-        novaTarefa.textContent = tarefas[i]
+
+        let textoTarefa = document.createElement('p')
+        textoTarefa.textContent = tarefas[i].texto
+
+        let novoPrazo = document.createElement('input')
+        novoPrazo.type = 'date'
+        novoPrazo.className = 'novo-prazo'
+        novoPrazo.value = tarefas[i].prazo
+
+        novoPrazo.addEventListener('change', () => {
+            tarefas[i].prazo = novoPrazo.value
+            atualizarListaStorage()
+        })
+
+        let novoStatus = document.createElement('input')
+        novoStatus.type = 'checkbox'
+        novoStatus.className = 'novo-status'
+        novoStatus.checked = tarefas[i].status
+        
+        novoStatus.addEventListener('change', () => {
+            tarefas[i].status = novoStatus.checked
+            novaTarefa.classList.toggle('concluida', novoStatus.checked)
+            atualizarListaStorage()
+        })
+        
 
         const botaoRemover = document.createElement('button')
         botaoRemover.className = 'botao-remover'
@@ -36,7 +79,7 @@ function renderizarTarefas() {
 
         // os parênteses de uma função são usados para entregar um valor de uma função para outra
         // nesse caso, foi usado o parênteses no removerTarefa() para levar o índice (i) de qual tarefa será excluída
-        // tem que colocar o tal valor na declaração da função, e quando for chamá-la para trabalhar
+        // tem que colocar o tal valor na declaração da função, e quando for chamá-la para rodar
         // quando colocamos o (i) na chamada e definição da função, nós levamos uma variável que era pra ser local
         // da função pai (que é o for) para virar local da função filho "removerTarefa()"
         // pense assim: “quando alguém clicar, eu vou usar este i aqui.”
@@ -54,12 +97,18 @@ function renderizarTarefas() {
 
         const botoes = document.createElement('div')
         botoes.className = 'botoes'
+        const divisao = document.createElement('div')
+        divisao.className = 'divisao'
 
+        novaTarefa.appendChild(textoTarefa)
+        botoes.appendChild(novoStatus)
         botoes.appendChild(botaoRemover)
         botoes.appendChild(botaoEditar)
         botaoRemover.appendChild(iconeRemover)
         botaoEditar.appendChild(iconeEditar)
-        novaTarefa.appendChild(botoes)
+        divisao.appendChild(botoes)
+        divisao.appendChild(novoPrazo)
+        novaTarefa.appendChild(divisao)
         listaTarefas.appendChild(novaTarefa)
     }
 }
@@ -69,23 +118,29 @@ function removerTarefa(i) { // (1/2) aqui, o "i" é um parâmetro, não tem valo
     // o (i, *) é o índice que o removerTarefa() trouxe para saber qual tarefa será excluída (sair do array pelo splice)
     mostrarBotao() // chama a função para atualizar o estado do botão quando apagar uma tarefa
     renderizarTarefas()
-    // por último, se renderiza a lista de tarefas novamente para mostrar a versão atualizada (sem a tarefa excluída).
+    // se renderiza a lista de tarefas novamente para mostrar a versão atualizada (sem a tarefa excluída).
+    atualizarListaStorage()
 }
 
 function editarTarefa(i) {
     let tarefaEditada = prompt("Escolha um novo nome para sua tarefa:")
     if (tarefaEditada.trim !== "") {
-        tarefas[i] = tarefaEditada
+        tarefas[i].texto = tarefaEditada
         renderizarTarefas()
+        atualizarListaStorage()
     }
 }
 
 function limparLista() {
     tarefas.length = 0
     mudarMensagemLimpar()
-    setTimeout(() => {mensagem.style.opacity = '0'},  2500)
     mostrarBotao() // chama a função para atualizar o estado do botão quando apagar todas as tarefas
     renderizarTarefas()
+    atualizarListaStorage()
+}
+
+function atualizarListaStorage() {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
 }
 
 function mostrarBotao() {
@@ -99,29 +154,32 @@ function mostrarBotao() {
 // inicializa estado do botão ao carregar o script
 mostrarBotao()
 
+function mudarMensagemErro() {
+    mensagem.textContent = 'Digite algo para adicionar uma tarefa.'
+    mensagem.classList.remove('sucesso', 'limpar')
+    mensagem.classList.add('erro')
+    mensagem.style.opacity = '1'
+    setTimeout(() => { mensagem.style.opacity = '0' }, 2500)
+}
+
+function mudarMensagemSucesso() {
+    mensagem.textContent = 'Tarefa adicionada com sucesso!'
+    mensagem.classList.remove('limpar', 'erro')
+    mensagem.classList.add('sucesso')
+    mensagem.style.opacity = '1'
+    setTimeout(() => { mensagem.style.opacity = '0' }, 2500)
+}
+
+function mudarMensagemLimpar() {
+    mensagem.textContent = 'Lista de tarefas limpa com sucesso!'
+    mensagem.classList.remove('sucesso', 'erro')
+    mensagem.classList.add('limpar')
+    mensagem.style.opacity = '1'
+    setTimeout(() => { mensagem.style.opacity = '0' }, 2500)
+}
+
 // Parâmetro é uma variável nova que recebe um valor no momento da execução da função.
 // É na definição da função, durante a execução.
 
 // Argumento é o valor que você passa para uma função quando a chama.
 // É na chamada da função, antes da execução.
-
-function mudarMensagemErro() {
-    mensagem.textContent = 'Digite algo para adicionar uma tarefa.'
-    mensagem.style.backgroundColor = '#d12727'
-    mensagem.style.color = '#fff'
-    mensagem.style.opacity = '1'
-}
-
-function mudarMensagemSucesso() {
-    mensagem.textContent = 'Tarefa adicionada com sucesso!'
-    mensagem.style.backgroundColor = '#26a01b'
-    mensagem.style.color = '#fff'
-    mensagem.style.opacity = '1'
-}
-
-function mudarMensagemLimpar() {
-    mensagem.textContent = 'Lista de tarefas limpa com sucesso!'
-    mensagem.style.backgroundColor = '#169957'
-    mensagem.style.color = '#fff'
-    mensagem.style.opacity = '1'
-}
